d48bcbb4e80ca452349134c6dddf0fd9614c72e8
Anthony <anthonnhani@gmail.com>
2026-03-16 14:41:41 -0600

    Set up vite and routing pages

---
+++ react/movie-page-tutorial/movie-page/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Favorites from './pages/Favorites';
+import Home from './pages/home'
+import {Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/favorites' element={<Favorites/>}></Route>
+      </Routes>
+    </main>
+  )
+}
+
+export default App
